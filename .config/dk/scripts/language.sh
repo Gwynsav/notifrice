17331f34b4c91b365e4f897efdd4eb77ca525f51
guu <104698661+Gwynsav@users.noreply.github.com>
2022-09-04 19:41:42 +0000

    Added WM config files and binds

---
+++ .config/dk/scripts/language.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+lang=$(setxkbmap -query | grep "layout:") && lang=${lang##* }
+[ "$lang" = "us" ] && lang="latam" || lang="us"
+[ "$lang" = "us" ] && stat="American English" || stat="Latinamerican Spanish"
+
+setxkbmap $lang
+dunstify -h string:x-dunst-stack-tag:lang \
+         -i "$HOME/.local/share/icons/Reversal-dark/actions/22/amarok_change_language.svg" \
+         "Keyboard Language" "Set to <b>$stat</b>"
